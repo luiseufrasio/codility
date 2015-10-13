74dbdb718cfceed6345715afe25b5db8f9dce567
luiseufrasio <luis.eufrasio@gmail.com>
2015-10-13 00:25:04 -0300

    little code refactor to make it simple

--- 2011/count_palindromic_slices.py
+++ 2011/count_palindromic_slices.py
@@ -2,13 +2,10 @@ def solution(s):
 	p = 0
 	n = len(s)
 	for i in range(2, n+1):
-		for j in range(0, n-1):
-			if j + i < n+1:
-				slice = s[j:j+i]
-				if slice == slice[::-1]:
-					p += 1
-			else:
-				break
+		for j in range(0, n + 1 -i):
+			slice = s[j:j+i]
+			if slice == slice[::-1]:
+				p += 1
 	return p;
 	
 if __name__ == '__main__':
