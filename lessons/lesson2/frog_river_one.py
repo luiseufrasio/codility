c8ef80d4fe83779b163a623fc864e19ac6aa81d2
luiseufrasio <luis.eufrasio@gmail.com>
2015-10-17 21:54:55 -0300

    Task 1 of Lesson 2 implemented

--- lessons/lesson2/frog_river_one.py
+++ lessons/lesson2/frog_river_one.py
@@ -1,2 +1,16 @@
-def solution(A):
-  pass
+def solution(X, A):
+	N = len(A)
+	B = [-1] * (X + 1)
+	for i in range(N):
+		j = A[i]
+		if B[j] == -1:
+			B[j] = i
+	
+	max_time = 0
+	for x in range(1, X + 1):
+		if B[x] == -1:
+			return -1
+		if B[x] > max_time:
+			max_time = B[x]  
+	
+	return max_time
---
+++ lessons/lesson2/tests.py
@@ -0,0 +1,44 @@
+import unittest
+from random import randint
+import time
+		
+class TestFrogRiverOne(unittest.TestCase):
+	def setUp(self):
+		from frog_river_one import solution
+		self.solution = solution
+
+	def test_x_one(self):
+		self.assertEqual(self.solution(1, [1, 1, 1, 1, 1, 1, 1, 1]), 0)
+
+	def test_x_two(self):
+		self.assertEqual(self.solution(2, [1, 1, 1, 1, 2, 1, 1, 1]), 4)
+	
+	def test_x_three(self):
+		self.assertEqual(self.solution(3, [1, 3, 1, 3, 2, 3, 3, 3]), 4)
+		
+	def test_x_four(self):
+		self.assertEqual(self.solution(4, [1, 3, 1, 4, 2, 3, 4, 4]), 4)
+		
+	def test_x_five(self):
+		self.assertEqual(self.solution(5, [1, 3, 1, 4, 2, 3, 5, 4]), 6)
+		
+	def test_minus_one(self):
+		self.assertEqual(self.solution(3, [1, 3, 1, 3, 3, 3, 3, 3]), -1)
+		self.assertEqual(self.solution(4, [1, 3, 1, 4, 3, 3, 4, 4]), -1)
+		self.assertEqual(self.solution(5, [1, 3, 1, 4, 3, 3, 5, 4]), -1)
+		
+	def test_twenty_thousand(self):
+		A = []
+		N = 100000
+		X = randint(1, N)
+		for i in range(N):
+			A.append(randint(1, X))
+		time_before = time.time()	
+		S = self.solution(X, A)
+		time_after = time.time()
+		self.assertTrue(time_after - time_before <= 1, 
+		"the time difference is %d" % (time_after - time_before))
+		print(time_after - time_before)
+				
+if __name__ == '__main__':
+	unittest.main()
