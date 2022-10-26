d317e7d2ad0cfc16fb05124169c35f15ce22f568
luiseufrasio <luis.eufrasio@gmail.com>
2022-10-25 21:53:18 -0300

    readability improved

--- python/lessons/1 - Iterations/tasks/bynary_gap.py
+++ python/lessons/1 - Iterations/tasks/bynary_gap.py
@@ -1,18 +1,18 @@
 # https://app.codility.com/demo/results/trainingJWBEAG-A8N/
 
 def solution(N):
-    max_bin_gap = 0
+    max_binary_gap = 0
     one_open = False
-    curr_bin_gap = 0
-    bin_N = bin(N)[2:]
-    for d in bin_N:
-        if d == '0' and one_open:
-            curr_bin_gap += 1
-        if d == '1':
+    current_binary_gap = 0
+    binary_number = bin(N)[2:]
+    for digit in binary_number:
+        if digit == '0' and one_open:
+            current_binary_gap += 1
+        if digit == '1':
             if one_open:
-                if curr_bin_gap > max_bin_gap:
-                    max_bin_gap = curr_bin_gap
-                curr_bin_gap = 0
+                if current_binary_gap > max_binary_gap:
+                    max_binary_gap = current_binary_gap
+                current_binary_gap = 0
             else:
                 one_open = True
-    return max_bin_gap
+    return max_binary_gap
